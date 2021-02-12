0a38c367e67b84d159a38d18b73e2f1b5bc475c2
Sam Hemphill <sam@meeum.com>
2021-02-13 10:59:43 +1100

    updagted logic

--- wp-content/themes/bm/page-work.php
+++ wp-content/themes/bm/page-work.php
@@ -4,7 +4,9 @@
 	<!-- section -->
 	<section>
 		<div class="inner">
-			<h1><?php the_title(); ?></h1>
+			<h1>
+				<?php the_title(); ?>
+			</h1>
 		</div>
 
 		<?php get_template_part( 'partials/header_blurb' ); ?>
@@ -70,65 +72,65 @@ $query = new WP_Query($args);
 			<?php
 if ( $query->have_posts() ) : ?>
 
-					<?php while ( $query->have_posts() ) : $query->the_post();?>
+			<?php while ( $query->have_posts() ) : $query->the_post();?>
 
-					<?php $colour_scheme = get_field( "post_colour_scheme" )?>
-
-
-					<article id="post-<?php the_ID(); ?>" <?php post_class( $colour_scheme ); ?>>
-
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-							<div class="inner">
-								<!-- post thumbnail -->
-								<?php if ( has_post_thumbnail() ) : // Check if thumbnail exists. ?>
-
-								<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
-
-								<?php endif; ?>
-								<!-- /post thumbnail -->
-
-								<!-- post title -->
-								<h2>
-									<?php the_title(); ?>
-								</h2>
-								<!-- /post title -->
+			<?php $colour_scheme = get_field( "post_colour_scheme" )?>
 
 
-								<?php /*
+			<article id="post-<?php the_ID(); ?>" <?php post_class( $colour_scheme ); ?>>
+
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					<div class="inner">
+						<!-- post thumbnail -->
+						<?php if ( has_post_thumbnail() ) : // Check if thumbnail exists. ?>
+
+						<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
+
+						<?php endif; ?>
+						<!-- /post thumbnail -->
+
+						<!-- post title -->
+						<h2>
+							<?php the_title(); ?>
+						</h2>
+						<!-- /post title -->
+
+
+						<?php /*
 
 
 	<!-- post details -->
 	<span class="date">
 		<time datetime="<?php the_time( 'Y-m-d' ); ?>
-								<?php the_time( 'H:i' ); ?>">
-								<?php the_date(); ?>
-								<?php the_time(); ?>
-								</time>
-								</span>
-								<span class="author">
-									<?php esc_html_e( 'Published by', 'html5blank' ); ?>
-									<?php the_author_posts_link(); ?>
-								</span>
-								<span class="comments">
-									<?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?>
-								</span>
-								<!-- /post details -->
+						<?php the_time( 'H:i' ); ?>">
+						<?php the_date(); ?>
+						<?php the_time(); ?>
+						</time>
+						</span>
+						<span class="author">
+							<?php esc_html_e( 'Published by', 'html5blank' ); ?>
+							<?php the_author_posts_link(); ?>
+						</span>
+						<span class="comments">
+							<?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?>
+						</span>
+						<!-- /post details -->
 
-								*/
-								?>
+						*/
+						?>
 
-								<?php the_excerpt(); // Build your custom callback length in functions.php. ?>
+						<?php the_excerpt(); // Build your custom callback length in functions.php. ?>
 
 
 
-								<?php //edit_post_link(); ?>
-							</div><!-- inner -->
-						</a>
-					</article>
-					<?php endwhile; ?>
-			
+						<?php //edit_post_link(); ?>
+					</div><!-- inner -->
+				</a>
+			</article>
+			<?php endwhile; ?>
 
-			
+
+
 
 			<?php else :?>
 			<?php //get_template_part( 'template-parts/content', 'none' );?>
