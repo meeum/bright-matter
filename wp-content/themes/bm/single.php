fc238434cd3d2c8f0cdbcf137fd555679d514deb
Sam Hemphill <sam@meeum.com>
2021-02-16 21:01:58 +1100

    single post template for clients

--- wp-content/themes/bm/single.php
+++ wp-content/themes/bm/single.php
@@ -1,13 +1,14 @@
 <?php get_header(); ?>
 
 <main role="main" aria-label="Content" data-template="single">
-	<!-- section -->
-	<section class="inner">
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<!-- article -->
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="inner">
 
 					<!-- post thumbnail -->
 					<?php if (has_post_thumbnail()) : // Check if Thumbnail exists. 
@@ -18,14 +19,32 @@
 						</a>
 					<?php endif; ?>
 					<!-- /post thumbnail -->
+				</div>
+				<!--inner-->
 
+
+				<div class="inner">
 					<!-- post title -->
 					<h1>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						<?php the_title(); ?>
 					</h1>
-					<!-- /post title -->
 
+				</div>
+				<!--inner-->
+
+				<?php get_template_part('partials/header_blurb'); ?>
+
+				<!-- /post title -->
+
+
+
+
+				<?php
+
+				/*
 					<!-- post details -->
+
+
 					<span class="date">
 						<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
 							<?php the_date(); ?> <?php the_time(); ?>
@@ -35,11 +54,32 @@
 					<span class="comments"><?php if (comments_open(get_the_ID())) comments_popup_link(__('Leave your thoughts', 'html5blank'), __('1 Comment', 'html5blank'), __('% Comments', 'html5blank')); ?></span>
 					<!-- /post details -->
 
+					*/
+
+				?>
+				<div class="inner">
 					<?php the_content(); // Dynamic Content. 
 					?>
 
-					<?php the_tags(__('Tags: ', 'html5blank'), ', ', '<br>'); // Separated by commas with a line break at the end. 
+					<?php
+					//the_tags(__('Tags: ', 'html5blank'), ', ', '<br>'); // Separated by commas with a line break at the end. 
+
+					$post_tags = get_the_tags();
+
+					if ($post_tags) {
+						echo '<ul class="client-tags">';
+						foreach ($post_tags as $tag) {
+							echo '<li>';
+							echo $tag->name . ' ';
+							echo '</li>';
+						}
+						echo '</ul>';
+					}
 					?>
+				</div>
+				<!--inner-->
+				<?php
+				/*
 
 					<p><?php esc_html_e('Categorised in: ', 'html5blank');
 						the_category(', '); // Separated by commas. 
@@ -54,25 +94,27 @@
 					<?php //comments_template(); 
 					?>
 
-				</article>
-				<!-- /article -->
-
-			<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<!-- article -->
-			<article>
-
-				<h1><?php esc_html_e('Sorry, nothing to display.', 'html5blank'); ?></h1>
+					*/
+				?>
 
 			</article>
 			<!-- /article -->
 
-		<?php endif; ?>
+		<?php endwhile; ?>
 
-	</section>
-	<!-- /section -->
+	<?php else : ?>
+
+		<!-- article -->
+		<article>
+
+			<h1><?php esc_html_e('Sorry, nothing to display.', 'html5blank'); ?></h1>
+
+		</article>
+		<!-- /article -->
+
+	<?php endif; ?>
+
+
 </main>
 
 <?php //get_sidebar(); 
