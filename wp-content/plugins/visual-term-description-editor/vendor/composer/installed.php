9fb4660686856873e10f7063e5b96d0b23acfbf1
Sam Hemphill <sam@meeum.com>
2021-02-23 20:24:02 +1100

    visual term description plugin for tag line breaks

---
+++ wp-content/plugins/visual-term-description-editor/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da98271041b75e9bc56454d838b29eb07975f95a',
+    'name' => 'sheabunge/visual-term-description-editor',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.9.0',
+      'version' => '1.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b93bcf0fa1fccb0b7d176b0967d969691cd74cca',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'sheabunge/visual-term-description-editor' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da98271041b75e9bc56454d838b29eb07975f95a',
+    ),
+  ),
+);
